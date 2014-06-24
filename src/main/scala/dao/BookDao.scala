7f169027c715c8d9b67d24340756c800bbdad97f
hittepit <fabrice.claes@gmail.com>
2014-06-24 13:31:25 +0200

    Corrections due to library refactoring

--- src/main/scala/dao/BookDao.scala
+++ src/main/scala/dao/BookDao.scala
@@ -4,13 +4,13 @@ import org.hittepit.smapapi.transaction.JdbcTransaction
 import org.hittepit.smapapi.transaction.TransactionManager
 import org.slf4j.LoggerFactory
 import org.hittepit.smapapi.core.result.Row
-import org.hittepit.smapapi.core.NotNullableString
+import org.hittepit.smapapi.core.StringProperty
 import model.BookType
-import org.hittepit.smapapi.core.NullableInt
+import org.hittepit.smapapi.core.OptionalIntProperty
 import model.Book
-import org.hittepit.smapapi.core.NullableString
+import org.hittepit.smapapi.core.OptionalStringProperty
 import org.hittepit.smapapi.core.Param
-import org.hittepit.smapapi.core.NotNullableInt
+import org.hittepit.smapapi.core.IntProperty
 import model.Isbn
 import org.hittepit.smapapi.core.Column
 
@@ -18,16 +18,16 @@ class BookDao(val transactionManager:TransactionManager) extends JdbcTransaction
 	val logger = LoggerFactory.getLogger(classOf[BookDao])
 
 	def bookMapper(row:Row) = {
-	  val bookType = BookType.withName(row.getColumnValue("BOOK_TYPE", NotNullableString))
-	  new Book(row.getColumnValue("ID", NullableInt), 
-	      row.getColumnValue("TITLE", NotNullableString), 
-	      Isbn(row.getColumnValue("ISBN", NotNullableString)), 
-	      row.getColumnValue("AUTHOR", NullableString), 
+	  val bookType = BookType.withName(row.getColumnValue("BOOK_TYPE", StringProperty))
+	  new Book(row.getColumnValue("ID", OptionalIntProperty), 
+	      row.getColumnValue("TITLE", StringProperty), 
+	      Isbn(row.getColumnValue("ISBN", StringProperty)), 
+	      row.getColumnValue("AUTHOR", OptionalStringProperty), 
 	      bookType)
 	}
 
 	def find(id:Int) = readOnly{session =>
-	  session.unique("select * from BOOK where id=?", List(Param(id,NotNullableInt)), bookMapper)
+	  session.unique("select * from BOOK where id=?", List(Param(id,IntProperty)), bookMapper)
 	}
 
 	def findAll = readOnly{session =>
@@ -38,8 +38,8 @@ class BookDao(val transactionManager:TransactionManager) extends JdbcTransaction
 	  book.id match {
 	  	case None => 
 	  	  val id = session.insert("insert into book (title,isbn,author,book_type) values (?,?,?,?)", 
-	  	    List(Param(book.title,NotNullableString),Param(book.isbn.code,NotNullableString),Param(book.author,NullableString),Param(book.bookType.toString,NotNullableString)), 
-	  	    Column("id",NotNullableInt))
+	  	    List(Param(book.title,StringProperty),Param(book.isbn.code,StringProperty),Param(book.author,OptionalStringProperty),Param(book.bookType.toString,StringProperty)), 
+	  	    Column("id",IntProperty))
 	  	  id match {
 	  	  	case Some(i) => new Book(Some(i),book.title,book.isbn,book.author,book.bookType)
 	  	  	case None => throw new Exception("No id generated")
@@ -47,18 +47,18 @@ class BookDao(val transactionManager:TransactionManager) extends JdbcTransaction
 	  	  
 	  	case Some(id) => 
 	  	  session.execute("update book set title=?, isbn=?, author=?, book_type=? where id=?",
-	  	      List(Param(book.title,NotNullableString),
-	  	          Param(book.isbn.code,NotNullableString),
-	  	          Param(book.author,NullableString),
-	  	          Param(book.bookType.toString,NotNullableString),
-	  	          Param(book.id.get,NotNullableInt)))
+	  	      List(Param(book.title,StringProperty),
+	  	          Param(book.isbn.code,StringProperty),
+	  	          Param(book.author,OptionalStringProperty),
+	  	          Param(book.bookType.toString,StringProperty),
+	  	          Param(book.id.get,IntProperty)))
 	  	  book
 	  }
 	}
 	
 	def delete(book:Book) = inTransaction{session =>
 	  book.id match {
-	    case Some(id) => session.execute("delete book where id=?",List(Param(id,NotNullableInt)))
+	    case Some(id) => session.execute("delete book where id=?",List(Param(id,IntProperty)))
 	    case None => throw new Exception("Transient book")
 	  }
 	}
--- src/main/scala/dao/BookDaoWithPropertyTypes.scala
+++ src/main/scala/dao/BookDaoWithPropertyTypes.scala
@@ -8,39 +8,39 @@ import model.BookType._
 import java.sql.PreparedStatement
 import java.sql.ResultSet
 import org.slf4j.LoggerFactory
-import org.hittepit.smapapi.core.NotNullableString
+import org.hittepit.smapapi.core.StringProperty
 import model.Isbn
 import model.Book
 import org.hittepit.smapapi.core.result.Row
-import org.hittepit.smapapi.core.NullableString
-import org.hittepit.smapapi.core.NullableInt
+import org.hittepit.smapapi.core.OptionalStringProperty
+import org.hittepit.smapapi.core.OptionalIntProperty
 import org.hittepit.smapapi.core.Param
-import org.hittepit.smapapi.core.NotNullableInt
+import org.hittepit.smapapi.core.IntProperty
 import org.hittepit.smapapi.core.Column
 
 object BookTypePropertyType extends PropertyType[BookType]{
-	def getColumnValue(rs: ResultSet, column: Either[String, Int]): BookType = BookType.withName(NotNullableString.getColumnValue(rs, column))
-	def setColumnValue(index: Int, value: BookType, ps: PreparedStatement): Unit= NotNullableString.setColumnValue(index, value.toString, ps)
+	def getColumnValue(rs: ResultSet, column: Either[String, Int]): BookType = BookType.withName(StringProperty.getColumnValue(rs, column))
+	def setColumnValue(index: Int, value: BookType, ps: PreparedStatement): Unit= StringProperty.setColumnValue(index, value.toString, ps)
 }
 
 object IsbnPropertyType extends PropertyType[Isbn]{
-	def getColumnValue(rs: ResultSet, column: Either[String, Int]): Isbn = Isbn(NotNullableString.getColumnValue(rs, column))
-	def setColumnValue(index: Int, value: Isbn, ps: PreparedStatement): Unit= NotNullableString.setColumnValue(index, value.code, ps)
+	def getColumnValue(rs: ResultSet, column: Either[String, Int]): Isbn = Isbn(StringProperty.getColumnValue(rs, column))
+	def setColumnValue(index: Int, value: Isbn, ps: PreparedStatement): Unit= StringProperty.setColumnValue(index, value.code, ps)
 }
 
 class BookDaoWithPropertyTypes(val transactionManager:TransactionManager) extends JdbcTransaction {
 	val logger = LoggerFactory.getLogger(classOf[BookDaoWithPropertyTypes])
 
 	def bookMapper(row:Row) = {
-	  new Book(row.getColumnValue("ID", NullableInt), 
-	      row.getColumnValue("TITLE", NotNullableString), 
+	  new Book(row.getColumnValue("ID", OptionalIntProperty), 
+	      row.getColumnValue("TITLE", StringProperty), 
 	      row.getColumnValue("ISBN", IsbnPropertyType), 
-	      row.getColumnValue("AUTHOR", NullableString), 
+	      row.getColumnValue("AUTHOR", OptionalStringProperty), 
 	      row.getColumnValue("BOOK_TYPE", BookTypePropertyType))
 	}
 	
 	def find(id:Int) = readOnly{session =>
-	  session.unique("select * from BOOK where id=?", List(Param(id,NotNullableInt)), bookMapper)
+	  session.unique("select * from BOOK where id=?", List(Param(id,IntProperty)), bookMapper)
 	}
 
 	def findAll = readOnly{session =>
@@ -50,11 +50,11 @@ class BookDaoWithPropertyTypes(val transactionManager:TransactionManager) extend
 	def persist(book:Book) = inTransaction{session =>
 	  require(! book.id.isDefined)
   	  val id = session.insert("insert into book (title,isbn,author,book_type) values (?,?,?,?)", 
-  	    List(Param(book.title,NotNullableString),
+  	    List(Param(book.title,StringProperty),
   	        Param(book.isbn,IsbnPropertyType),
-  	        Param(book.author,NullableString),
+  	        Param(book.author,OptionalStringProperty),
   	        Param(book.bookType,BookTypePropertyType)), 
-  	    Column("id",NotNullableInt))
+  	    Column("id",IntProperty))
   	  id match {
   	  	case Some(i) => new Book(Some(i),book.title,book.isbn,book.author,book.bookType)
   	  	case None => throw new Exception("No id generated")
@@ -64,17 +64,17 @@ class BookDaoWithPropertyTypes(val transactionManager:TransactionManager) extend
 	def update(book:Book) = inTransaction{session =>
 	  require(book.id.isDefined)
   	  session.execute("update book set title=?, isbn=?, author=?, book_type=? where id=?",
-  	      List(Param(book.title,NotNullableString),
+  	      List(Param(book.title,StringProperty),
   	          Param(book.isbn,IsbnPropertyType),
-  	          Param(book.author,NullableString),
+  	          Param(book.author,OptionalStringProperty),
   	          Param(book.bookType,BookTypePropertyType),
-  	          Param(book.id.get,NotNullableInt)))
+  	          Param(book.id.get,IntProperty)))
   	  book
 	}
 	
 	def delete(book:Book) = inTransaction{session =>
 	  book.id match {
-	    case Some(id) => session.execute("delete book where id=?",List(Param(id,NotNullableInt)))
+	    case Some(id) => session.execute("delete book where id=?",List(Param(id,IntProperty)))
 	    case None => throw new Exception("Transient book")
 	  }
 	}}
